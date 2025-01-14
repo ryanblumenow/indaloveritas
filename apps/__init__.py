68e0f054dca1f45b575c55784faaa7601e14ebfd
Ryan <blumenowster@gmail.com>
2025-01-14 19:11:48 +0200

    Changing problematic imports in apps init

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,7 +1,7 @@
 # from apps.home_app import HomeApp
 from apps.login_app_veritas import LoginApp
-from apps.cheat_app import CheatApp
-from apps.myloading_app import MyLoadingApp
-from apps.signup import SignUpApp
-from apps.cookie_cutter import CookieCutterApp
-from apps.load_app import LoaderTestApp
+# from apps.cheat_app import CheatApp
+# from apps.myloading_app import MyLoadingApp
+# from apps.signup import SignUpApp
+# from apps.cookie_cutter import CookieCutterApp
+# from apps.load_app import LoaderTestApp
